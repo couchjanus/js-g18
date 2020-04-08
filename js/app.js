1e6bb9e127a1c9a291c5bc40b0e5fcba40e5d02b
couch Janus <couchjanus@gmail.com>
2020-04-08 13:38:48 +0300

    added unit 3

---
+++ js/app.js
@@ -0,0 +1,30 @@
+// jquery app.js
+
+$(function () {
+    // $('.shopping__cart').click(function(){
+    // console.log("jQuery: It's Shopping Cart");
+    // });
+
+    // $('.shopping__cart').on('click', function(){
+    //     console.log("jQuery: It's Shopping Cart");
+    // });
+
+    // $('.shopping__cart').on('click', function(){
+    //     $(".cart-overlay").addClass("transparentBcg");
+    //     $(".cart").addClass("showCart");
+    // });
+
+    // $('.close__btn').on('click', function(){
+    //     $(".cart-overlay").removeClass("transparentBcg");
+    //     $(".cart").removeClass("showCart");
+    // });
+
+    // $('.add_to_card').on('click', function () {
+    //     let y = 180;
+    //     $('.product__image').css('transform','rotateY(' + y + 'deg)');
+    //     let val = $('.shopping__cart-items').text();
+    //     val = +(val)+1;
+    //     $('.shopping__cart-items').text(val);
+    //     $('.item-amount').text(val);
+    //  });
+});
--- js/index.js
+++ js/index.js
@@ -1,16 +1,18 @@
 // index.js
 (function(){
 
-    // function openCart() {
-    //     document.getElementById("sidebar").style.right = '0px';
-    // }
-      
-    // function closeCart() {
-    //     document.getElementById("sidebar").style.right = "-100%";
-    // }
+    function openCart() {
+        document.querySelector(".cart-overlay").classList.add("transparentBcg");
+        document.querySelector(".cart").classList.add("showCart");
+    }
 
-    // document.querySelector(".close__btn").onclick=closeCart;
-    // document.querySelector(".shopping__cart").onclick=openCart;
+    function closeCart() {
+        document.querySelector(".cart-overlay").classList.remove("transparentBcg");
+        document.querySelector(".cart").classList.remove("showCart");
+    }
+
+    document.querySelector(".close__btn").addEventListener("click", closeCart);
+    document.querySelector(".shopping__cart").addEventListener("click", openCart);
 
     let navbarItem = document.querySelectorAll(".navbar__item");
    
@@ -26,5 +28,44 @@
             }
         }   
     });
+    
+    // shopping__cart-items
+    document.querySelector(".add_to_card").addEventListener("click", function () {
+        let y = 180;
+        document.querySelector('.product__image').style.transform = 'rotateY(' + y + 'deg)';
+        let val = document.querySelector('.shopping__cart-items').innerText;
+        val = +(val)+1;
+        document.querySelector('.shopping__cart-items').innerText = val;
+        document.querySelector('.item-amount').innerText = val;
+    });
+
+    // fa-chevron-up
+    let plus = document.querySelector('.fa-chevron-up');
+    plus.addEventListener('click', function(e) {
+        console.log(document.querySelector('.item-amount').innerText);
+        let val = document.querySelector('.item-amount').innerText;
+        val = +(val)+1;
+        document.querySelector('.item-amount').innerText = val;
+        updateTotal();
+        
+    });
+
+    // fa-chevron-down
+    let minus = document.querySelector('.fa-chevron-down');
+    minus.addEventListener('click', function(e) {
+        console.log(document.querySelector('.item-amount').innerText);
+        let val = document.querySelector('.item-amount').innerText;
+        val = +(val)-1;
+        document.querySelector('.item-amount').innerText = val;
+        updateTotal();
+    });
+
+    function updateTotal() {
+        let total = 0
+        let amount = document.querySelector('.item-amount').innerText;
+        let price = document.querySelector('.item-price').innerText;
+        total = amount * price;
+        document.querySelector('.cart-total').innerText = total;
+    }
+    
 })();
- 
